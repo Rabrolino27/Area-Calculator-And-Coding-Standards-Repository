ba2d30f405732b128e5ba03ac6d8ac6f15d5adcc
Rabrolino27 <107254791+Rabrolino27@users.noreply.github.com>
2024-02-26 07:43:57 +0100

    Update ShapeCalculator.php

--- app/Contracts/ShapeCalculator.php
+++ app/Contracts/ShapeCalculator.php
@@ -4,11 +4,11 @@ namespace App\Contracts;
 
 interface ShapeCalculator
 {
-    public function surface():string;
+    public function surface():integer;
 
-    public function diameter():string;
+    public function diameter():integer;
 
-    public function circumference():string;
+    public function circumference():integer;
 
     //Note: Parameters are not declared in interface to allow expansion & scalability in the future as the solution for both shapes requires different kind of parameters 
 }
