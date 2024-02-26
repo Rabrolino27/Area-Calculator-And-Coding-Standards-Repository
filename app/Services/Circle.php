81880959563e4d4addd15b449122e3838e39db76
Rabrolino27 <107254791+Rabrolino27@users.noreply.github.com>
2024-02-26 07:43:13 +0100

    Update Circle.php

--- app/Services/Circle.php
+++ app/Services/Circle.php
@@ -10,18 +10,21 @@ class Circle implements ShapeCalculator
         $this->radius = $radius;
         $this->pie = 3.14;
     }
-    public function surface():string{
+    public function surface():integer
+    {
         $area = pow($this->radius, 2);
         $surface = $area * $this->pie;
         return $surface;
     }
 
-    public function diameter():string{
+    public function diameter():integer
+    {
         $diamater = 2 * $this->radius;
         return $diamater;
     }
 
-    public function circumference():string{
+    public function circumference():integer
+  {
         $circumference = 2 * $this->pie * $this->radius;
         return $circumference;
     }
