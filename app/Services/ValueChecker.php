8bbc6a6559ac20e707ad0120033c90a03098d388
Rabrolino27 <107254791+Rabrolino27@users.noreply.github.com>
2024-02-26 07:53:36 +0100

    Update ValueChecker.php

--- app/Services/ValueChecker.php
+++ app/Services/ValueChecker.php
@@ -1,1 +1,14 @@
+<?php
+namespace App\Services;
 
+class ValueChecker
+{
+    public function check(array $values): bool
+    {
+        $filteredValues = array_filter($values, function ($value) {
+            return $value < 0;
+        });
+
+        return empty($filteredValues);
+    }
+}
