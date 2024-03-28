819d013ddf1b362537c15cf2656d9a5e4c93eaf4
Rabrolino27 <107254791+Rabrolino27@users.noreply.github.com>
2024-03-28 16:02:31 +0100

    Update GeomentaryCalculator.php

--- app/Contracts/GeomentaryCalculator.php
+++ app/Contracts/GeomentaryCalculator.php
@@ -4,7 +4,7 @@ namespace App\Contracts;
 
 interface GeomentaryCalculator
 {
-    public function sumOfArea(array $arr):nonsense;
+    public function sumOfArea(array $arr):integer;
 
     public function sumOfDiameter(array $arr):integer;
 }
